ef9d1dd77e835c427ab98963a730999f01388a25
urrri <yury.go@gmail.com>
2018-06-24 20:14:26 +0300

    [file] -> [name]

--- test/fixtures/jsx-map-class-props/format - literal classes/expected.js
+++ test/fixtures/jsx-map-class-props/format - literal classes/expected.js
@@ -3,7 +3,7 @@ const x = <div>
   <div />
   <div className="e2e-aaa" />
   <div className="e2e-bbb" />
-  <div className="format---literal-classes_-file-_ccc_3PCfnwJn" />
+  <div className="format---literal-classes_actual_ccc_3PCfnwJn" />
   <div className="eee" />
   <div className="yyy e2e-xxx" />
 
--- test/fixtures/jsx-map-class-props/format - non-literal classes/expected.js
+++ test/fixtures/jsx-map-class-props/format - non-literal classes/expected.js
@@ -2,7 +2,7 @@ import _getClassName from 'babel-plugin-jsx-map-class-props/dist/browser/getClas
 const x = <div>
 
   <div className={_getClassName('a', 'e2e-|')} />
-  <div className={_getClassName('b', 'format---non-literal-classes_-file-_|_a7UkV-M-')} />
+  <div className={_getClassName('b', 'format---non-literal-classes_actual_|_a7UkV-M-')} />
   <div className={'c'} />
 
 </div>;
